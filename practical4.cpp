95abc5fa2adbeaeb153f34998b35ad2aa1b2885b
vamshikrishna1429 <jalasutrapu.krishna130368@marwadiuniversity.ac.in>
2026-07-29 13:20:30 +0530

    Add files via upload

---
+++ practical4.cpp
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main() {
+    int n, i;
+    long long fact = 1;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    for(i = 1; i <= n; i++) {
+        fact = fact * i;
+    }
+
+    printf("Factorial of %d = %lld\n", n, fact);
+
+    return 0;
+}
+#include <stdio.h>
+
+long long factorial(int n) {
+    if(n == 0 || n == 1)
+        return 1;
+    else
+        return n * factorial(n - 1);
+}
+
+int main() {
+    int n;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    printf("Factorial of %d = %lld\n", n, factorial(n));
+
+    return 0;
+}
